e26ad7b6b93f403af6717d0fa565828459fdc895
AslanHuseynov <64832203+AslanHuseynov@users.noreply.github.com>
2020-08-27 17:02:40 +0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+    /*Responsive Navigation*/
+   $(document).ready(function(){
+  $('.toggle').click(function(){
+   $('.toggle').toggleClass('active')
+   
+   $('nav ul').toggleClass('active-menu')
+  
+   
+  })
+ });
+ 
